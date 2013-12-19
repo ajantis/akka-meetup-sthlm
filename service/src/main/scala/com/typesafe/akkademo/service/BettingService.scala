b0306dc4728b4b0e785e5b79540be0cbfe597502
Dmitry Ivanov <id.ajantis@gmail.com>
2013-12-19 11:38:16 +0100

    Service implementation

--- service/src/main/scala/com/typesafe/akkademo/service/BettingService.scala
+++ service/src/main/scala/com/typesafe/akkademo/service/BettingService.scala
@@ -3,10 +3,15 @@
  */
 package com.typesafe.akkademo.service
 
-import akka.actor.{ ActorLogging, Actor }
-import com.typesafe.akkademo.common.{ Bet, RetrieveBets }
+import akka.actor.{ ActorRef, ActorLogging, Actor }
+import com.typesafe.akkademo.common._
+import scala.concurrent.duration._
+import com.typesafe.akkademo.common.PlayerBet
+import scala.Some
 
 class BettingService extends Actor with ActorLogging {
+
+  import BettingService._
 
   /**
    * TASKS:
@@ -19,8 +24,56 @@ class BettingService extends Actor with ActorLogging {
    * Keep any message locally until there is a processor service available
    */
 
+  val ActiveTime = 2000L
+  var processor: Option[(Long, ActorRef)] = None
+  var currentSequenceId: Int = 0
+  var bets: Map[Int, PlayerBet] = Map()
+
+  import context.dispatcher
+  context.system.scheduler.schedule(2 seconds, 2 seconds, self, ProcessUnhandledBets)
+
   def receive = {
-    case bet: Bet     ⇒
-    case RetrieveBets ⇒
+    case bet: Bet ⇒
+      val pb = createPlayerBet(bet)
+      bets = bets.updated(pb.id, pb)
+      getProcessor.foreach(_.forward(pb))
+
+    case cmd @ RetrieveBets ⇒
+      getProcessor.foreach(_.forward(cmd))
+
+    case RegisterProcessor       ⇒ updateProcessor(sender)
+
+    case ProcessUnhandledBets    ⇒ processUnhandledBets()
+
+    case ConfirmationMessage(id) ⇒ bets = bets - id
+
   }
+
+  private def createPlayerBet(b: Bet): PlayerBet = PlayerBet(nextSeqId(), b)
+
+  private def nextSeqId(): Int = {
+    currentSequenceId += 1
+    currentSequenceId
+  }
+
+  private def getProcessor: Option[ActorRef] =
+    processor.collect {
+      case (lastActive, p) if System.currentTimeMillis() - lastActive < ActiveTime ⇒ p
+    }
+
+  private def updateProcessor(p: ActorRef): Unit = {
+    log.info("Processor is alive!")
+    processor = Some((System.currentTimeMillis(), p))
+  }
+
+  private def processUnhandledBets(): Unit = {
+    log.info(s"We have ${bets.size} unhandled bets..")
+    getProcessor.foreach { p ⇒
+      bets.values.foreach { p ! _ }
+    }
+  }
+}
+
+object BettingService {
+  case object ProcessUnhandledBets
 }
